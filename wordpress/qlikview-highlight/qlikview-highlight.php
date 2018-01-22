00da40e753c5966aefd74e13496120e69771b73b
MattFryer <matthew_fryer@hotmail.com>
2018-01-22 13:02:49 +0000

    Solved issue with clashing with some other shortcode plugins

--- wordpress/qlikview-highlight/qlikview-highlight.php
+++ wordpress/qlikview-highlight/qlikview-highlight.php
@@ -30,42 +30,90 @@
 
 defined('ABSPATH') or die("No script kiddies please!"); //Block direct access to this php file
 
-//Add the necessary highlight code to the HTML header for the site
+// Add the necessary highlight code to the HTML header for the site
 function qlikview_highlight_head() {
     echo '<link rel="stylesheet" title="QlikView" href="' . plugin_dir_url(__FILE__) . 'css/qlikview.css">';
-	echo '<script src="' . plugin_dir_url(__FILE__) . 'js/highlight.pack.js"></script>';
-	echo '<script>';
-	echo '	hljs.configure({tabReplace: \'    \'});';
-	echo '	hljs.initHighlightingOnLoad();';
-	echo '</script>';
+		echo '<script src="' . plugin_dir_url(__FILE__) . 'js/highlight.pack.js"></script>';
+		echo '<script>';
+		echo '	hljs.configure({tabReplace: \'    \'});';
+		echo '	hljs.initHighlightingOnLoad();';
+		echo '</script>';
 }
 add_action('wp_head', 'qlikview_highlight_head');
 
-//Make Wordpress process the post text after the shortcodes have been processed
-//Without this Wordpress will process the code because it won't yet be inside the pre and code tags
-remove_filter( 'the_content', 'wpautop' );
-add_filter( 'the_content', 'wpautop' , 12);
-remove_filter( 'the_content', 'wptexturize' );
-add_filter( 'the_content', 'wptexturize', 12 );
-
 // Add QlikView specific shortcode [qlikview]...[/qlikview]
-//Accepts type parameter [qlikview type="qvs"].
-//Options are: 
-//	"qvs" or "qlikview-script" or "qv-script" - Qlikview Script (default)
-//	"exp" or "qlikview-exp" or "qv-exp" - QlikView Expression
-//	"sql" - SQL
-//  "vbscript" - Visual Basic Script
-//	"javascript" - Java Script
+// Accepts type parameter [qlikview type="qvs"].
+// Options are: 
+//	 "qvs" or "qlikview-script" or "qv-script" - Qlikview Script (default)
+//	 "exp" or "qlikview-exp" or "qv-exp" - QlikView Expression
+//	 "sql" - SQL
+//   "vbscript" - Visual Basic Script
+//	 "javascript" - Java Script
 function qlikview_highlight_shortcode( $atts , $content = null ) { 
-	//obtain the passed type (script or expression) if any. Defaults to script if not specified
+	// obtain the passed type (script or expression) if any. Defaults to script if not specified
 	$attributes = shortcode_atts( array(
         'type' => 'qvs'
     ), $atts );
 	return '<pre><code class="' . $a['type'] . '">' . $content . '</code></pre>';
 }
-add_shortcode( 'qlikview', 'qlikview_highlight_shortcode' );
 
-//Add the button to the TinyMCE so that the shortcode can be added via the visual page/post editor
+function qlikview_pre_process_shortcode($content) {
+    global $shortcode_tags;
+ 
+    // Backup current registered shortcodes and clear them all out
+    $orig_shortcode_tags = $shortcode_tags;
+    $shortcode_tags = array();
+ 
+    // Add the shortcode
+		add_shortcode("qlikview","qlikview_highlight_shortcode");
+ 
+    // Do the shortcode (only the one above is registered)
+    $content = do_shortcode($content);
+ 
+    // Put the original shortcodes back
+    $shortcode_tags = $orig_shortcode_tags;
+ 
+    return $content;
+}
+add_filter('the_content', 'qlikview_pre_process_shortcode', 7);
+
+// Add the button to the text editor
+function qlikview_button_script() {
+    if(wp_script_is("quicktags")) {
+        ?>
+            <script type="text/javascript">
+                // this function is used to retrieve the selected text from the text editor
+                function getSel()
+                {
+                    var txtarea = document.getElementById("content");
+                    var start = txtarea.selectionStart;
+                    var finish = txtarea.selectionEnd;
+                    return txtarea.value.substring(start, finish);
+                }
+
+                QTags.addButton( 
+                    "qlikview_shortcode", 
+                    "Qlik", 
+                    callback
+                );
+
+                function callback()
+                {
+                    var selected_text = getSel();
+										var type = prompt("Type (qvs, exp, sql, vbscript, javascript)", "qvs");
+										if (type == null || type == '' || (type != 'qvs' && type != 'exp' && type != 'sql' && type != 'vbscript' && type != 'javascript')){
+											var type = 'qvs';
+										}
+                    QTags.insertContent("[qlikview type=\"" + type + "\"]" +  selected_text + "[/qlikview]");
+                }
+            </script>
+        <?php
+    }
+}
+
+add_action("admin_print_footer_scripts", "qlikview_button_script");
+
+// Add the button to the TinyMCE so that the shortcode can be added via the visual page/post editor
 function register_qlikview_highlight_button( $buttons ) {
    array_push( $buttons, "|", "qlikview" );
    return $buttons;
