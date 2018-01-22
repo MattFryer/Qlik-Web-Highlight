c954d5eb4c5cee9c6b6077ad04d3c71d2d9c0eea
MattFryer <matthew_fryer@hotmail.com>
2018-01-22 22:46:35 +0000

    Updated batch file to build before moving

--- development/tools/move build file.bat
+++
@@ -1,3 +0,0 @@
-copy ..\build\highlight.pack.js ..\..\mediawiki\QlikView\resources
-copy ..\build\highlight.pack.js ..\..\qvhighlight\qvhighlight
-copy ..\build\highlight.pack.js ..\..\wordpress\qlikview-highlight\js
