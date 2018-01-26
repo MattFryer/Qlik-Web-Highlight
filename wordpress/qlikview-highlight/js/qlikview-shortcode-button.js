6760b2089e755e9eca59f958ce74cf140e9f6ce1
MattFryer <matthew_fryer@hotmail.com>
2018-01-26 11:53:55 +0000

    Corrected issue with missing TinyMCE button

--- wordpress/qlikview-highlight/js/qlikview-shortcode-button.js
+++ wordpress/qlikview-highlight/js/qlikview-shortcode-button.js
@@ -1,36 +1,36 @@
 (function() {
-   tinymce.create('tinymce.plugins.qlikcode', {
-      init : function(ed, url) {
-         ed.addButton('qlik_code_button', {
-            title : 'Qlik Code',
-            image : url+'/qlikview.png',
-            onclick : function() {
-			   var selected = tinyMCE.activeEditor.selection.getContent( {format : "text"} );
-			   if (selected == null || selected == '') {
-			      var selected = 'Your code here...';
-			   }
-			   
-               var type = prompt("Type (qvs, exp, sql, vbscript, javascript)", "qvs");
-			   if (type == null || type == '' || (type != 'qvs' && type != 'exp' && type != 'sql' && type != 'vbscript' && type != 'javascript')){
-			      var type = 'qvs';
-			   }
-
-               ed.execCommand('mceInsertContent', false, '[qlik-code type="'+type+'"]' + selected + '[/qlik]');
-            }
-         });
-      },
-      createControl : function(n, cm) {
-         return null;
-      },
-      getInfo : function() {
-         return {
-            longname : "Qlik for WordPress",
-            author : 'Matthew Fryer',
-            authorurl : 'http://www.qlikviewaddict.com/',
-            infourl : 'http://www.qlikviewaddict.com/',
-            version : "1.2"
-         };
-      }
-   });
-   tinymce.PluginManager.add('qlik_code_button', tinymce.plugins.qlikcode);
+  tinymce.create('tinymce.plugins.qlik_code_button', {
+    init : function(ed, url) {
+      ed.addButton('qlik_code_button', {
+        title : 'Qlik Code',
+        image : url+'/qlikview.png',
+        onclick : function() {
+					var selected = tinyMCE.activeEditor.selection.getContent( {format : "text"} );
+					if (selected == null || selected == '') {
+						var selected = 'Your code here...';
+					}
+		  
+					var type = prompt("Type (qvs, exp, sql, vbscript, javascript)", "qvs");
+					if (type == null || type == '' || (type != 'qvs' && type != 'exp' && type != 'sql' && type != 'vbscript' && type != 'javascript')){
+						var type = 'qvs';
+					}
+               
+					ed.execCommand('mceInsertContent', false, '[qlik-code type="'+type+'"]' + selected + '[/qlik]');
+         }
+      });
+    },
+    createControl : function(n, cm) {
+      return null;
+    },
+    getInfo : function() {
+      return {
+        longname : "Qlik for WordPress",
+        author : 'Matthew Fryer',
+        authorurl : 'http://www.qlikviewaddict.com/',
+        infourl : 'http://www.qlikviewaddict.com/',
+        version : "1.2"
+      };
+    }
+  });
+  tinymce.PluginManager.add('qlik_code_button', tinymce.plugins.qlik_code_button);
 })();
