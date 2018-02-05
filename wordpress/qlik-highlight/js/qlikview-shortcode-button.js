be41dfede87d9c0c9c4f247e0e7a21ca611da955
MattFryer <matthew_fryer@hotmail.com>
2018-02-05 01:16:01 +0000

    Removed unneccessary spoces spaces

--- wordpress/qlik-highlight/js/qlikview-shortcode-button.js
+++ wordpress/qlik-highlight/js/qlikview-shortcode-button.js
@@ -11,7 +11,7 @@
 					if (selectedContent === null || selectedContent === "") {
 						selectedContent = "Your code here...";
 					}
-		  
+					
 					var codeType = prompt("codeType (qvs, exp, sql, vbscript, javascript)", "qvs");
 					if (codeType === null || codeType === "" || (codeType !== "qvs" && codeType !== "exp" && codeType !== "sql" && codeType !== "vbscript" && codeType !== "javascript")){
 						codeType = "qvs";
