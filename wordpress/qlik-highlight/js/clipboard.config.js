98b6398dcee5ab02762ef448a6463cc79d366919
MattFryer <matthew_fryer@hotmail.com>
2018-02-06 12:28:09 +0000

    Code improvements

--- wordpress/qlik-highlight/js/clipboard.config.js
+++ wordpress/qlik-highlight/js/clipboard.config.js
@@ -1,9 +1,8 @@
 var snippets=document.querySelectorAll(".qlik-highlight-pre");
 
 [].forEach.call(snippets,function(snippet){
-	//snippet.firstChild.insertAdjacentHTML("beforebegin","<button class=\"btn\" data-clipboard-snippet><img class=\"clippy\" width=\"13\" src=\"" + qlikHighlightClipboardConfig.pluginsUrl + "js/clippy.svg\" alt=\"Copy to clipboard\"></button>");
-	snippet.firstChild.insertAdjacentHTML("beforebegin", Sanitizer.escapeHTML("<button class=\"btn\" data-clipboard-snippet><img class=\"clippy\" width=\"13\" src=\"" + qlikHighlightClipboardConfig.pluginsUrl + "js/clippy.svg\" alt=\"Copy to clipboard\"></button>"));
-	//Sanitizer.escapeHTML`<a href="${link}">click</a>`
+	var baseURL = qlikHighlightClipboardConfig.pluginsUrl;
+	snippet.firstChild.insertAdjacentHTML("beforebegin", Sanitizer.escapeHTML`<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="${baseURL}js/clippy.svg" alt="Copy to clipboard"></button>`);
 });
 
 var clipboardSnippets=new Clipboard("[data-clipboard-snippet]",
