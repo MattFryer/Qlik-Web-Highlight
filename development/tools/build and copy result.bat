7ea743e3b7d834b35e838844cc7208b695ab7c20
MattFryer <matthew_fryer@hotmail.com>
2018-02-13 00:35:31 +0000

    Enter to close batfile window

--- development/tools/build and copy result.bat
+++ development/tools/build and copy result.bat
@@ -3,3 +3,4 @@ node tools/build.js qlikview-script qlikview-expression sql vbscript javascript
 copy build\highlight.pack.js ..\mediawiki\QlikView\resources
 copy build\highlight.pack.js ..\qvhighlight\qvhighlight
 copy build\highlight.pack.js ..\wordpress\qlik-highlight\js
+set /p finish=Hit enter to finish: 
