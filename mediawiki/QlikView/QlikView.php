1920a62890bade0cf0201f5c916ed7a4a6d454d0
Matt Fryer <MattFryer@users.noreply.github.com>
2019-03-01 09:50:30 +0000

    Removed die() call

--- mediawiki/QlikView/QlikView.php
+++ mediawiki/QlikView/QlikView.php
@@ -1,39 +1,35 @@
 <?php
- // This is not a valid entry point to MediaWiki.
-if (!defined('MEDIAWIKI')) {
-    die('To install QlikView Syntax Highlight, put the following line in LocalSettings.php: require_once( "\$IP/extensions/QlikView/QlikView.php" );');
-}
+ // Run only if this is a valid entry point to MediaWiki.
+if (defined('MEDIAWIKI')) {
 
-// Take credit for this extension.
-$wgExtensionCredits['other'][] = array(
-    'name' => 'QlikView for WordPress',
-    'author' => 'Matt Fryer',
-    'url' => 'http://www.qlikviewaddict.com/',
-    'description' => 'Automatically syntax highlights QlikView script and expressions on any MediaWiki page.',
-    'descriptionmsg' => 'qlikview-ext-desc',
-    'version' => '0.1',
-);
+  // Take credit for this extension.
+  $wgExtensionCredits['other'][] = array(
+     'name' => 'QlikView for WordPress',
+     'author' => 'Matt Fryer',
+     'url' => 'http://www.qlikviewaddict.com/',
+     'description' => 'Automatically syntax highlights QlikView script and expressions on any MediaWiki page.',
+     'descriptionmsg' => 'qlikview-ext-desc',
+     'version' => '0.1',
+  );
 
-$dir = __DIR__ . '/';
+  $dir = __DIR__ . '/';
 
-// Register the extension's main code/class.
-$wgAutoloadClasses['QlikView'] = $dir . 'QlikView.body.php';
+  // Register the extension's main code/class.
+  $wgAutoloadClasses['QlikView'] = $dir . 'QlikView.body.php';
 
-// Register our internationalization file.
-$wgExtensionMessagesFiles['QlikView'] = $dir . 'QlikView.i18n.php';
+  // Register our internationalization file.
+  $wgExtensionMessagesFiles['QlikView'] = $dir . 'QlikView.i18n.php';
 
-// Let MediaWiki know about the new tag.
-$wgHooks['ParserFirstCallInit'][] = 'QlikView::init';
+  // Let MediaWiki know about the new tag.
+  $wgHooks['ParserFirstCallInit'][] = 'QlikView::init';
 
-// Register hook to get items into the header.
-$wgHooks['BeforePageDisplay'][] = 'QlikView::onBeforePageDisplay';
+  // Register hook to get items into the header.
+  $wgHooks['BeforePageDisplay'][] = 'QlikView::onBeforePageDisplay';
 
-// ResourceLoader modules
-$wgResourceModules['ext.QlikView'] = array(
-    'localBasePath' => __DIR__,
-    'remoteExtPath' => 'QlikView',
-    'styles' => array('resources/qlikview.css'),
-    'scripts' => array('resources/highlight.pack.js', 'resources/highight.init.js')
-);
-
- 
+  // ResourceLoader modules
+  $wgResourceModules['ext.QlikView'] = array(
+     'localBasePath' => __DIR__,
+     'remoteExtPath' => 'QlikView',
+     'styles' => array('resources/qlikview.css'),
+     'scripts' => array('resources/highlight.pack.js', 'resources/highight.init.js')
+  );
