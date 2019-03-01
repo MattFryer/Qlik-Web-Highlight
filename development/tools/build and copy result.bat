112c9ec129aa7a4567db716dd3d76a8fed3a9abf
Matt Fryer <MattFryer@users.noreply.github.com>
2019-03-01 12:27:42 +0000

    Added CSS copy to standardise across packages

--- development/tools/build and copy result.bat
+++ development/tools/build and copy result.bat
@@ -1,6 +1,12 @@
 cd ..
+REM Buid the code
 node tools/build.js qlikview-script qlikview-expression sql vbscript javascript css xml
+REM Copy the package to the relevant folders
 copy build\highlight.pack.js ..\mediawiki\QlikView\resources
 copy build\highlight.pack.js ..\qvhighlight\qvhighlight
 copy build\highlight.pack.js ..\wordpress\qlik-highlight\js
+REM Copy the latest CSS packages
+copy src\styles\qlikview.css ..\mediawiki\QlikView\resources
+copy src\styles\qlikview.css ..\qvhighlight\qvhighlight
+copy src\styles\qlikview.css ..\wordpress\qlik-highlight\css
 set /p finish=Hit enter to finish: 
